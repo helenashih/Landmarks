a251dcd49fcebb69bb53bc77add6cd85df609fbb
helenashih <42580758+helenashih@users.noreply.github.com>
2021-01-21 17:44:19 -0500

    Initial commit for Swift demo

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.black)
+                HStack {
+                        Text("Joshua Tree National Park")
+                            .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title3)
+                Text("Turtle Rock is a rock formation in Joshua Tree National Park, California.")
+                    .font(.footnote)
+                Spacer()
+                Text("This massive formation is located across from the parking lot from the entrance into Real Hidden Valley area in Joshua Tree National Park. The northwest face of Turtle Rock is several hundred feet long and works like a massive wall protecting a pleasant picnic area with trees, benches, and even bathrooms. On any weekend during the climbing season, you may see several climbing classes in progress under the trees and around many boulders strewn on the base of the northwest face. You will also run into families picnicking and children scrambling on smaller low angle rocks.")
+                    .font(.footnote)
+            }
             .padding()
+        }
     }
 }
 
---
+++ Shared/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Helena Chapman on 1/21/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.134_728, longitude: -116.313_0661),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
